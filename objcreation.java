44ddc558c48e5698e32e1600527869f801d9dfcc
RidhaAnsar <140969987+RidhaAnsar@users.noreply.github.com>
2024-02-07 19:31:39 +0530

    Add files via upload

---
+++ objcreation.java
@@ -0,0 +1,28 @@
+import java.io.*;
+
+class person {
+    int age;
+    String name;
+
+    public void setperson(int a, String n) {
+        age = a;
+        name = n;
+    }
+
+    public void dispperson() {
+        System.out.println("Name: " + name + " Age: " + age);
+    }
+}
+
+public class objcreation {
+    public static void main(String[] args) {
+        person p, q;
+        p = new person();
+        q = new person();
+        p.setperson(20, "ABC");
+        q.setperson(25, "DDD");
+        p.dispperson();
+        q.dispperson();
+
+    }
+}
