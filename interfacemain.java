415fe06737e22091498aa3113f5a862d49d67615
Ridha Ansar <140969987+RidhaAnsar@users.noreply.github.com>
2024-02-24 00:38:26 +0530

    Add files via upload

---
+++ interfacemain.java
@@ -0,0 +1,23 @@
+import java.io.*;
+interface i1 {
+public void disp();
+public void print();
+}
+class c1 implements i1 {
+public void disp() {
+System.out.println("Interface disp");
+}
+public void print() {
+System.out.println("Interface print");
+}
+public void draw() {
+System.out.println("Draw not in Interface"); 
+}
+}
+class interfacemain {
+public static void main(String args[]) {
+c1 obj=new c1();
+obj.disp();
+obj.print();
+obj.draw();
+} }
