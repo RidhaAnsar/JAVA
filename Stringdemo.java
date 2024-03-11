e15d0a85bc718ca8f76de0e0528386ad86f535ea
Ridha Ansar <140969987+RidhaAnsar@users.noreply.github.com>
2024-03-11 20:18:52 +0530

    Add files via upload

---
+++ Stringdemo.java
@@ -0,0 +1,12 @@
+class Stringdemo
+{
+public static void main( String arg [])
+{
+String s1 = "Rajagiri";
+String s2=new String("Java");
+System.out.println("Length is    "+s1.length());
+System.out.println("Lowercase   "+s1.toLowerCase());
+System.out.println("uppercase    "+s2.toUpperCase());
+System.out.println("concat    "+s1.concat(s2));
+}
+}
