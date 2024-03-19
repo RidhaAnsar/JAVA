5bf136e7b80f2b32d692e8a6ef6fdf44a9e389a8
Ridha Ansar <140969987+RidhaAnsar@users.noreply.github.com>
2024-03-19 23:45:13 +0530

    InterfaceSample.java

---
+++ InterfaceSample.java
@@ -0,0 +1,58 @@
+// Define an interface
+interface Shape {
+    double getArea();
+    double getPerimeter();
+}
+
+// Implement the interface in a class
+class Circle implements Shape {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * Math.PI * radius;
+    }
+}
+
+// Implement the interface in another class
+class Rectangle implements Shape {
+    private double width;
+    private double height;
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public double getArea() {
+        return width * height;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * (width + height);
+    }
+}
+
+public class InterfaceSample {
+    public static void main(String[] args) {
+        Shape circle = new Circle(5);
+        Shape rectangle = new Rectangle(4, 6);
+
+        System.out.println("Circle Area: " + circle.getArea());
+        System.out.println("Circle Perimeter: " + circle.getPerimeter());
+
+        System.out.println("Rectangle Area: " + rectangle.getArea());
+        System.out.println("Rectangle Perimeter: " + rectangle.getPerimeter());
+    }
+}
