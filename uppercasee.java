187569fd0effa6143dd913e04152b785b0dbd52c
Ridha Ansar <140969987+RidhaAnsar@users.noreply.github.com>
2024-03-23 01:04:59 +0530

    uppercasee.java

---
+++ uppercasee.java
@@ -0,0 +1,7 @@
+public class uppercasee{
+  public static void main(String[] args) {
+    String myStr = "       Hello World!        ";
+    System.out.println(myStr);
+    System.out.println(myStr.trim());
+  }
+}
