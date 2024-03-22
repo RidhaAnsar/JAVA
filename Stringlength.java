43fc7105593360a158c7e09b436fab0056faa321
Ridha Ansar <140969987+RidhaAnsar@users.noreply.github.com>
2024-03-23 00:56:29 +0530

    Stringlength.java

---
+++ Stringlength.java
@@ -0,0 +1,9 @@
+public class Stringlength
+{
+    public static void main(String[] args)
+    {
+        String name= "educative";
+        int length = name.length();
+        System.out.println("length of string "+name+" is " +length);
+    }
+}  
