bc5c7fcdd9f0ee20fba1c302a152b59d4f8a8b5d
Ridha Ansar <140969987+RidhaAnsar@users.noreply.github.com>
2024-05-24 16:34:57 +0530

    VowelCount.java

---
+++ VowelCount.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class VowelCount {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+        int vowelCount = 0;
+        for (char c : input.toLowerCase().toCharArray()) {
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                vowelCount++;
+            }
+        }
+        System.out.println("Number of vowels: " + vowelCount);
+    }
+}
