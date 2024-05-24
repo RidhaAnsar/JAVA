ec843384b0a556db6e5cdbcf7c4e5cd8f24b7582
Ridha Ansar <140969987+RidhaAnsar@users.noreply.github.com>
2024-05-24 22:37:09 +0530

    ArmstrongNumber.java

---
+++ ArmstrongNumber.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class ArmstrongNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        int originalNumber = number;
+        int result = 0;
+
+        while (originalNumber != 0) {
+            int digit = originalNumber % 10;
+            result += Math.pow(digit, 3);
+            originalNumber /= 10;
+        }
+
+        if (result == number) {
+            System.out.println(number + " is an Armstrong number.");
+        } else {
+            System.out.println(number + " is not an Armstrong number.");
+        }
+    }
+}
