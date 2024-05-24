97bbf348a35f2ea9b7dba8da3558b920c96b25bf
Ridha Ansar <140969987+RidhaAnsar@users.noreply.github.com>
2024-05-24 16:35:32 +0530

    WordCount.java

---
+++ WordCount.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class WordCount {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+        String[] words = input.trim().split("\\s+");
+        System.out.println("Number of words: " + words.length);
+    }
+}
